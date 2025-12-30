fa0738f32fa29f67e9e53c5f3f2d52e12589de79
mayank <mac@macs-MacBook-Pro.local>
2025-12-31 02:05:44 +0530

    pushing comming

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         This is new version changes mayank 02
         </a>
       </header>
     </div>
